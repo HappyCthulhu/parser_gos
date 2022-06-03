f66440b4d399f37a31f1d4bd5beca295a54365ca
Валерий Газаров <v.gazarov@fortus.pro>
2022-06-03 16:20:06 +0300

    Add get_status, get_link_to_purcase funcs
    Add another locator to get_purcases func

--- pages/main.py
+++ pages/main.py
@@ -8,13 +8,25 @@ class MainPage:
     def __init__(self):
         self.link = 'https://zakupki.gov.ru/epz/order/extendedsearch/results.html'
 
+
     def get_tree(self):
         html_text = requests.get(self.link).text
         self.tree = html.document_fromstring(html_text)
 
     def get_purchases(self):
-        self.purchases = self.tree.xpath(MainPageLocators.purchases)
+        self.purchases = self.tree.xpath(MainPageLocators.divs_purchases)
 
-    def get_link_to_purchases(self, purchase_tree):
-        test = purchase_tree.xpath('//div[@class="registry-entry__body-value"]/text()')
-        print()
+    def get_status(self, purchase):
+        #  TODO: функция, которая html возвращает
+        purchase_tree = html.tostring(purchase)
+        purchase_obj = html.document_fromstring(purchase_tree)
+        status = purchase_obj.xpath(MainPageLocators.text_status)[0].lstrip().rstrip()
+        return status
+
+    def get_link_to_purchases(self, purchase):
+        # TODO: может некостыльный метод есть?
+        purchase_tree = html.tostring(purchase)
+        purchase_obj = html.document_fromstring(purchase_tree)
+        link = (f'https://zakupki.gov.ru{purchase_obj.xpath(MainPageLocators.text_links)[0]}')
+        return link
+
