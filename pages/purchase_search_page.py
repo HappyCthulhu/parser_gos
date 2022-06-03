18e3d997fd94bd158176614960eda1ab647458b8
Валерий Газаров <v.gazarov@fortus.pro>
2022-06-03 18:22:08 +0300

    Move function call in __init__

--- pages/purchase_search_page.py
+++ pages/purchase_search_page.py
@@ -5,19 +5,21 @@ from locators import MainPageLocators
 
 
 class PurchaseSearchPage:
-    def __init__(self):
-        self.link = 'https://zakupki.gov.ru/epz/order/extendedsearch/results.html'
+    def __init__(self, link):
+        self.link = link
+        self.tree = self.get_tree()
+        self.purchases = self.get_purchases()
 
     def find_number_of_pages(self):
-        pages_count = self.tree.xpath(MainPageLocators.text_pages_number)[-1]
+        pages_count = int(self.tree.xpath(MainPageLocators.text_pages_number)[-1])
         return pages_count
 
     def get_tree(self):
         html_text = requests.get(self.link).text
-        self.tree = html.document_fromstring(html_text)
+        return html.document_fromstring(html_text)
 
     def get_purchases(self):
-        self.purchases = self.tree.xpath(MainPageLocators.divs_purchases)
+        return self.tree.xpath(MainPageLocators.divs_purchases)
 
     def get_status(self, purchase):
         #  TODO: функция, которая html возвращает
