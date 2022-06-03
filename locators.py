895e41853faf15534014fea0bf3e83c5b6b0cec3
Валерий Газаров <v.gazarov@fortus.pro>
2022-06-03 16:17:11 +0300

    Add MainPageLocators

--- locators.py
+++ locators.py
@@ -1,6 +1,15 @@
 class MainPageLocators:
-    purchases = "// div[@class ='search-registry-entry-block box-shadow-search-input']"
+    divs_purchases = "//div[@class ='search-registry-entry-block box-shadow-search-input']"
+    text_links = '//div[@class="registry-entry__header-mid__number"]/a/@href'
+    text_status = '//div[@class="registry-entry__header-mid__title text-normal"]/text()'
+    button_next_page = '//a[@class="paginator-button paginator-button-next"]'
+    text_pages_number = '//span[@class="link-text"]'
 
-# class PurchasePageLocators:
+
+# pageNumber = 150
+# recordsPerPage=_50
+
+class PurchasePageLocators:
+    slkdfj = 'lsdkjf'
 
 
