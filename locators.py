5116c881a26d388f90377b1cb63868af57cf8588
Валерий Газаров <v.gazarov@fortus.pro>
2022-06-13 20:13:01 +0300

    Change text_ktru_position_code locator
    В некоторых случаях парсер не забирал код ктру. Пофиксил.

--- locators.py
+++ locators.py
@@ -16,20 +16,16 @@ class PurchasePageLocators:
     text_starting_price = '//*[contains(text(),"Начальная цена")]/following::*[1]/text()'
     # оператор AND приделать внутри Xpath.
     # Ибо сейчас он ищет только по слову "начала",
-    # а надо еще и по "дата". Чтоб по двум словам подряд искал.
+    # а надо еще и по "дата". Чтоб по двум словам подряд искал
     # При этом, чтоб они могли соять в рандомном порядке внутри одного элемента.
 
     text_date_and_time_of_the_application_beginning = '//*[contains(text(),"начала ")]/following::*[1]/text()[1]'
     text_date_and_time_of_the_application_deadline = '//*[contains(text(),"Дата и время окончания срока подачи заявок")]/following::*[1]/text()[1]'
     text_date_of_the_procedure_for_submitting_proposals = '//*[contains(text(),"Дата проведения процедуры подачи предложений о цене контракта либо о сумме цен единиц товара, работы, услуги")]/following::*[1]/text()[1]'
     text_timezone = '(//span[@class="timeZoneName"])[1]/text()'
-    # TODO: добавить в названия переменных локаторов вид переменных (text, button etc)
-    # TODO: вместе звездочки приделать //  ?
     blocks_ktru = '//div[@id="positionKTRU"]//tbody[@class="tableBlock__body"]/tr[@class="tableBlock__row"]'
-    # TODO: может стоит обрабатывать каждый блок закупки отдельно, создавая для него html?
-    text_ktru_position_code = '//tr/td[2]/*/text()'
+    text_ktru_position_code = '//tr/td[2]//text()'
     text_ktru_name_of_product_or_service = '//tr/td[3]/text()'
-    # TODO: посмотреть, какие точно позиции КТРУ нужно собрать
     text_ktru_count = '//tr/td[5]/text()'
     text_ktru_sum_cost = '//span[@class="cost"]/text()'
     text_purchase_number = '//a[contains(text(),"№")]/text()'
