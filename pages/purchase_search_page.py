d76cf709a22322e90e7c6b597fd95b0d935603fc
Валерий Газаров <v.gazarov@fortus.pro>
2022-06-19 17:05:10 +0300

    Add create_link func

--- pages/purchase_search_page.py
+++ pages/purchase_search_page.py
@@ -1,13 +1,45 @@
 from locators import MainPageLocators
+from logger_settings import logger
 
 from pages.base_page import BasePage
 
 
 class PurchaseSearchPage(BasePage):
-    def __init__(self, link):
-        self.link = link
+    def __init__(self, main_link, link_params: dict):
+        self.link = self.create_link(main_link, link_params)
         self.tree = self.get_tree(self.link)
         self.purchases = self.get_purchases()
+
+    def create_link(self, main_link, link_params):
+
+        logger.debug(link_params)
+        link = f'{main_link}?&search-filter=Дате+размещения&sortBy=UPDATE_DATE'
+
+        if link_params.get("search_string"):
+            link = f'{link}&searchString={link_params["search_string"]}'
+
+        if link_params.get("page_number"):
+            link = f'{link}&pageNumber={link_params["page_number"]}'
+
+        if link_params.get("publish_date_from"):
+            link = f'{link}&publishDateFrom={link_params["publish_date_from"]}'
+
+        if link_params.get("publish_date_to"):
+            link = f'{link}&publishDateTo={link_params["publish_date_to"]}'
+
+        if link_params.get("publish_date_from"):
+            link = f'{link}&publishDateFrom={link_params["publish_date_from"]}'
+
+        if link_params.get("price_from_general"):
+            link = f'{link}&priceFromGeneral={link_params["price_from_general"]}'
+
+        if link_params.get("price_to_general"):
+            link = f'{link}&priceToGeneral={link_params["price_to_general"]}'
+
+        if link_params.get("records_per_page"):
+            link = f'{link}&recordsPerPage={link_params["records_per_page"]}'
+
+        return link
 
     def find_number_of_pages(self):
         pages_count = int(self.tree.xpath(MainPageLocators.text_pages_number)[-1])
