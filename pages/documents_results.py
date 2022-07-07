83064ee4757ab4394fa8d5772069b7b9af9219c1
Валерий Газаров <v.gazarov@fortus.pro>
2022-07-07 20:00:21 +0400

    Add .doc proccessing, another way to find ru

--- pages/documents_results.py
+++ pages/documents_results.py
@@ -1,7 +1,10 @@
+import re
+
 import requests
 from docx.api import Document
 
 from locators import PurchaseSupplierResultsLocators, DocumentsResultsLocators
+from logger_settings import logger
 from pages.base_page import BasePage
 
 
@@ -23,10 +26,21 @@ class DocumentsResults(BasePage):
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
         }
         response = requests.get(url, allow_redirects=True, headers=headers)
+        fname = re.findall("filename=(.+)", response.headers['content-disposition'])[0]
+        if '.docx' in fname:
+            fname = "doc.docx"
+        elif '.doc' in fname:
+            fname = "doc.doc"
+            logger.debug('.doc format')
+        else:
+            logger.critical(f"Неизвестный формат файла:{fname}")
 
         # TODO: статичный путь сделать
-        with open("doc.docx", "wb") as file:
+        with open(fname, "wb") as file:
             file.write(response.content)
+
+        return fname
+
 
     def get_contract_file(self, doc_block_tree):
         if not self.check_element_existing(DocumentsResultsLocators.a_contract_file, doc_block_tree):
@@ -41,11 +55,15 @@ class DocumentsResults(BasePage):
             for cell in row.cells:
                 text = cell.text
                 if 'Номер регистрационного удостоверения' in text:
-                    # https://zakupki.gov.ru/epz/order/notice/ea20/view/common-info.html?regNumber=0322100001622000024
-                    # test = text.split('Номер регистрационного удостоверения: ')
                     text = text.split('Номер регистрационного удостоверения: ')[1].lstrip().rstrip()
                     if text not in ru:
                         ru.append(text)
+                else:
+                    text = text.split('/')
+                    # проверяем, что символы по обе стороны от слеша являются цифрами
+                    if len(text) == 2:
+                        if text[0][-1].isdigit() and text[1][0].isdigit():
+                            ru.append(''.join(text))
 
         return ru
 
@@ -54,9 +72,16 @@ class DocumentsResults(BasePage):
             for row in table.rows:
                 for cell in row.cells:
                     text = cell.text
-                    # if 'Номер регистрационного удостоверения' in text:
                     if '№ п/п' in text:
                         return table
+                    elif 'Номер регистрационного удостоверения' in text:
+                        return table
+                    text = text.split('/')
+                    # проверяем, что символы по обе стороны от слеша являются цифрами
+                    if len(text) == 2:
+                        if text[0][-1].isdigit() and text[1][-1].isdigit():
+                            return table
+
 
         return ''
 
@@ -94,11 +119,16 @@ class DocumentsResults(BasePage):
             else:
                 doc_link = self.get_contract_file(doc_block_tree)
                 if doc_link:
-                    self.download_doc(doc_link)
-                    document = Document('doc.docx')
-                    specification_table = self.find_specification_table(document.tables)
-                    if specification_table:
-                        ru = self.get_ru_from_table(specification_table.rows)
+                    doc_name = self.download_doc(doc_link)
+                    try:
+                        document = Document(doc_name)
+                        specification_table = self.find_specification_table(document.tables)
+                        if specification_table:
+                            ru = self.get_ru_from_table(specification_table.rows)
+                            return ru
+                    except ValueError:
+                        logger.info('Error, related to .doc file. Skip oppening')
+                        ru = []
                         return ru
                     else:
                         return []
@@ -107,3 +137,4 @@ class DocumentsResults(BasePage):
                     self.ru_number = []
         else:
             return []
+
